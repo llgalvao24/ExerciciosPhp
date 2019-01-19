a07151bfd9dba3e20f85efc21e6251c97e0443d5
Luiz G. Galvão <31831085+llgalvao24@users.noreply.github.com>
2019-01-19 00:14:47 -0200

    Implements the test testForTinyWords

--- tests/src/Unit/TextWrapTest.php
+++ tests/src/Unit/TextWrapTest.php
@@ -18,6 +18,7 @@ class TextWrapTest extends TestCase {
   public function setUp() {
     $this->resolucao = new Resolucao();
     $this->baseString = "Se vi mais longe foi por estar de pé sobre ombros de gigantes";
+    $this->tinyBaseString = "Se me viu e não deu oi, não sei o que foi";
   }
 
   /**
@@ -66,5 +67,29 @@ class TextWrapTest extends TestCase {
     $this->assertEquals("ombros de", $ret[4]);
     $this->assertEquals("gigantes", $ret[5]);
   }
+  
+  **
+   * Testa a quebra de linha para palavras muito curtas.
+   *
+   * @covers Galoa\ExerciciosPhp\TextWrap\Resolucao::textWrap
+   */
+  public function testForTinyWords() {
+    $ret = $this->resolucao->textWrap($this->tinyBaseString, 4);
+    $this->assertCount(12, $ret);
+    $this->assertEquals("Se", $ret[0]);
+    $this->assertEquals("me", $ret[1]);
+    $this->assertEquals("viu", $ret[2]);
+    $this->assertEquals("e", $ret[3]);
+    $this->assertEquals("não", $ret[4]);
+    $this->assertEquals("deu", $ret[5]);
+    $this->assertEquals("oi,", $ret[6]);
+    $this->assertEquals("não", $ret[7]);
+    $this->assertEquals("sei", $ret[8]);
+    $this->assertEquals("o", $ret[9]);
+    $this->assertEquals("que", $ret[10]);
+    $this->assertEquals("foi", $ret[11]);
+  }
+  
+  
 
 }
